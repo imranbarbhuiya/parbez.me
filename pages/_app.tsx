14d6917799f6d6119ef7c1f619d652ec790267d6
parbez <imranbarbhuiya.fsd@gmail.com>
2022-09-12 13:02:10 +0530

    chore: change lint deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import NextNProgress from 'nextjs-progressbar';
 
-import '@fontsource/poppins';
 import 'windi.css';
 import '../styles/globals.css';
 
@@ -14,26 +13,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<div>
 			<Head>
-				<title>Sofi Card Game</title>
-				<meta name="title" content="Sofi Card Game" />
+				<title>Parbez</title>
+				<meta name="title" content="Parbez - A Developer" />
 				<link rel="icon" type="image/png" href="/favicon.png" />
-				<meta
-					name="description"
-					content="A fun card collecting and battling game with over 40,000 characters to collect. It contains cards from anime, k-pop, pc games, pokemon, cartoons, original characters, and much more"
-				/>
-				<meta
-					name="keywords"
-					content="discord,freegames,genshin,valorant,karuta,tofu,anime,sofi,game,best,collect,gachapon,players,gems,wist,wisteria,sho,anime,games,more,fun,friends,multiplayer,to,play,fun2play"
-				/>
+				<meta name="description" content="A Developer who loves to code" />
+				<meta name="keywords" content="developer,webdeveloper,backenddeveloper" />
 				<meta name="robots" content="index, follow" />
 				<meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
 				<meta name="language" content="English" />
-				<meta name="author" content="Sho" />
-
-				<meta property="twitter:card" content="summary_large_image" />
-
-				<meta property="og:image" content="/embed.png" />
-				<meta property="twitter:image" content="/embed.png" />
+				<meta name="author" content="Parbez" />
 			</Head>
 			<MantineProvider
 				theme={{
