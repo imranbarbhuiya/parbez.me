48400ad7ce87b18cfab3033714fac23881c39a5e
parbez <imranbarbhuiya.fsd@gmail.com>
2022-09-12 12:40:55 +0530

    chore: add missing deps

--- windi.config.ts
+++ windi.config.ts
@@ -4,22 +4,16 @@ import { defineConfig } from 'windicss/helpers';
 import TypographyPlugin from 'windicss/plugin/typography';
 
 export default defineConfig({
-  extract: {
-    include: ['**/*.{jsx,tsx,css}'],
-    exclude: ['node_modules', '.git', '.next'],
-  },
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        'open-sans': ['Open Sans', 'Roboto', 'sans-serif'],
-        poppins: ['Poppins', 'Open Sans', 'sans-serif'],
-        'maven-pro': ['Maven Pro', 'sans-serif'],
-      },
-      animation: {
-        'spin-slow': 'spin 3s linear infinite',
-      },
-    },
-  },
-  plugins: [ScrollbarPlugin, AnimationsPlugin, TypographyPlugin()],
+	extract: {
+		include: ['**/*.{jsx,tsx,css}'],
+		exclude: ['node_modules', '.git', '.next'],
+	},
+	theme: {
+		extend: {
+			animation: {
+				'spin-slow': 'spin 3s linear infinite',
+			},
+		},
+	},
+	plugins: [ScrollbarPlugin, AnimationsPlugin, TypographyPlugin()],
 });
