6c12f11ff0239675a0812d46b1e6086d1519cb08
parbez <imranbarbhuiya.fsd@gmail.com>
2024-08-31 16:20:53 +0530

    test new card

--- app/3d/page.tsx
+++ app/3d/page.tsx
@@ -1,147 +1,123 @@
-/* eslint-disable react/no-unknown-property */
 'use client';
 
-import { OrbitControls, Sphere, MeshDistortMaterial, Points, PointMaterial, Text3D, Center } from '@react-three/drei';
-import { Canvas, useFrame, extend } from '@react-three/fiber';
-import React, { useRef, useMemo, useEffect } from 'react';
-import { type Mesh, type Group, type Object3DEventMap, type Points as ThreePoints } from 'three';
-import { FontLoader } from 'three/addons/loaders/FontLoader.js';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js';
+/* eslint-disable react/no-unknown-property */
+import { Text, RoundedBox, PerspectiveCamera, OrbitControls, Stars } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef, useMemo } from 'react';
 
-extend({ TextGeometry });
+import type * as THREE from 'three';
 
-function ParticleBackground() {
-	const ref = useRef<ThreePoints>(null);
-	const particleCount = 2_000;
-	const particles = useMemo(() => {
-		const temp = [];
-		for (let i = 0; i < particleCount; i++) {
-			const x = (Math.random() - 0.5) * 20;
-			const y = (Math.random() - 0.5) * 20;
-			// eslint-disable-next-line id-length
-			const z = (Math.random() - 0.5) * 20;
-			temp.push(x, y, z);
+function Card() {
+	const mesh = useRef<THREE.Group>(null);
+
+	const shaderArgs = useMemo(
+		() => ({
+			uniforms: {
+				time: { value: 0 },
+			},
+			vertexShader: `
+		varying vec2 vUv;
+		varying vec3 vNormal;
+		void main() {
+		  vUv = uv;
+		  vNormal = normal;
+		  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 		}
-		return new Float32Array(temp);
-	}, []);
-
-	useFrame((_state, delta) => {
-		if (!ref.current) return;
-		ref.current.rotation.x -= delta / 20;
-		ref.current.rotation.y -= delta / 25;
-		const positions = ref.current.geometry.attributes.position.array;
-		for (let i = 0; i < positions.length; i += 3) {
-			positions[i] += (Math.random() - 0.5) * 0.01;
-			positions[i + 1] += (Math.random() - 0.5) * 0.01;
-			positions[i + 2] += (Math.random() - 0.5) * 0.01;
+	  `,
+			fragmentShader: `
+		uniform float time;
+		varying vec2 vUv;
+		varying vec3 vNormal;
+		
+		float noise(vec2 p) {
+		  return fract(sin(dot(p, vec2(12.9898, 78.233))) * 43758.5453);
 		}
-		ref.current.geometry.attributes.position.needsUpdate = true;
-	});
-
-	return (
-		<group rotation={[0, 0, Math.PI / 4]}>
-			<Points frustumCulled={false} positions={particles} ref={ref} stride={3}>
-				<PointMaterial color="#fff" depthWrite={false} size={0.02} sizeAttenuation transparent />
-			</Points>
-		</group>
+		
+		void main() {
+		  vec3 lightDir = normalize(vec3(1.0, 1.0, 1.0));
+		  vec3 normal = normalize(vNormal);
+		  
+		  float noiseValue = noise(vUv * 50.0 + time * 0.1);
+		  vec3 normalOffset = vec3(noiseValue * 0.1 - 0.05);
+		  normal += normalOffset;
+		  normal = normalize(normal);
+		  
+		  float diffuse = max(dot(normal, lightDir), 0.0);
+		  vec3 baseColor = vec3(0.23, 0.1, 0.7);
+		  vec3 highlightColor = vec3(0.3, 0.4, 0.5);
+		  gl_FragColor = vec4(mix(baseColor, highlightColor, diffuse + noiseValue * 0.1), 1.0);
+		}
+	  `,
+		}),
+		[],
 	);
-}
 
-function AnimatedSphere() {
-	const mesh = useRef<Mesh>(null);
-	useFrame((_state, delta) => {
-		if (!mesh.current) return;
-		mesh.current.rotation.x += delta * 0.25;
-		mesh.current.rotation.y += delta * 0.25;
-	});
-
-	return (
-		<Sphere args={[1, 100, 200]} ref={mesh} scale={2.5} visible>
-			<MeshDistortMaterial
-				attach="material"
-				color="#8352FD"
-				distort={0.3}
-				opacity={0.8}
-				roughness={0}
-				speed={1.5}
-				transparent
-			/>
-		</Sphere>
-	);
-}
-
-function Title() {
-	const mesh = useRef<Mesh>(null);
 	useFrame((state) => {
 		if (mesh.current) mesh.current.rotation.y = Math.sin(state.clock.elapsedTime) * 0.2;
 	});
 
 	return (
-		<group position={[0, 0.5, 0]}>
-			<Center>
-				<Text3D curveSegments={12} font="/fonts/helvetiker_regular.typeface.json" height={0.1} ref={mesh} size={0.5}>
-					Imran Barbhuiya
-					<meshStandardMaterial color="#ffffff" />
-				</Text3D>
-			</Center>
+		<group ref={mesh}>
+			<RoundedBox args={[3.5, 5, 0.1]} radius={0.1} smoothness={4}>
+				<shaderMaterial args={[shaderArgs]} attach="material" />
+			</RoundedBox>
+			<Text
+				anchorX="center"
+				anchorY="middle"
+				color="#C0C0C0"
+				// font="https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmSU5vAw.ttf"
+				fontSize={0.4}
+				position={[0, 2, 0.06]}
+			>
+				Parbez
+			</Text>
+			<Text
+				anchorX="center"
+				anchorY="middle"
+				color="#C0C0C0"
+				// font="https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxK.ttf"
+				fontSize={0.4}
+				position={[0, 0.5, 0.06]}
+			>
+				Imran Barbhuiya
+			</Text>
+			<Text
+				anchorX="center"
+				anchorY="middle"
+				color="#A0A0A0"
+				// font="https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmSU5vAw.ttf"
+				fontSize={0.1}
+				position={[0, 0, 0.06]}
+				textAlign="center"
+			>
+				{`I'm a passionate Full Stack Developer
+			with a strong drive for innovation
+			and continuous learning.`}
+			</Text>
+			<Text
+				anchorX="center"
+				anchorY="middle"
+				color="#A0A0A0"
+				// font="https://fonts.gstatic.com/s/roboto/v27/KFOlCnqEu92Fr1MmSU5vAw.ttf"
+				fontSize={0.15}
+				position={[0, -2, 0.06]}
+			>
+				FullStack Web and Mobile Developer
+			</Text>
 		</group>
 	);
 }
 
-function SocialIcon({
-	position,
-	scale,
-	url,
-	children,
-}: {
-	readonly children: React.ReactNode;
-	readonly position: [number, number, number];
-	readonly scale: number;
-	readonly url: string;
-}) {
-	const mesh = useRef<Group<Object3DEventMap>>(null);
-	useFrame((state) => {
-		if (mesh.current) mesh.current.rotation.y = Math.sin(state.clock.elapsedTime) * 0.2;
-	});
-
+export default function Component() {
 	return (
-		<group position={position} ref={mesh} scale={scale}>
-			<mesh onClick={() => window.open(url, '_blank')}>
-				{children}
-				<meshStandardMaterial color="#ffffff" />
-			</mesh>
-		</group>
-	);
-}
-
-export default function HeroSection() {
-	useEffect(() => {
-		const loader = new FontLoader();
-
-		loader.load('/fonts/helvetiker_regular.typeface.json');
-	}, []);
-	return (
-		<div className="relative h-screen w-full">
-			<Canvas className="absolute inset-0">
+		<div className="h-screen w-full bg-black">
+			<Canvas>
 				<color args={['#000000']} attach="background" />
+				<PerspectiveCamera makeDefault position={[0, 0, 6]} />
 				<ambientLight intensity={0.5} />
-				<spotLight angle={0.15} penumbra={1} position={[10, 10, 10]} />
-				<pointLight position={[-10, -10, -10]} />
-				<ParticleBackground />
-				<group position={[0, 0, -1]}>
-					<AnimatedSphere />
-					<Title />
-					<SocialIcon position={[-1, -0.8, 0]} scale={0.3} url="https://github.com/imranbarbhuiya">
-						<Text3D curveSegments={12} font="/fonts/helvetiker_regular.typeface.json" height={0.2} size={1}>
-							Github
-						</Text3D>
-					</SocialIcon>
-					<SocialIcon position={[1, -0.8, 0]} scale={0.3} url="https://x.com/notparbez">
-						<Text3D curveSegments={12} font="/fonts/helvetiker_regular.typeface.json" height={0.2} size={1}>
-							X
-						</Text3D>
-					</SocialIcon>
-				</group>
+				<pointLight intensity={0.8} position={[10, 10, 10]} />
+				<Card />
+				<Stars count={5_000} depth={50} factor={4} fade radius={100} saturation={0} speed={1} />
 				<OrbitControls enableZoom={false} />
 			</Canvas>
 		</div>
